b2c88ea46b09f02f83cc1325d2585fef80b4c1e0
Seoester <seoester@googlemail.com>
2020-01-03 17:50:04 +0100

    Refactor docker entry.sh script
    Any user-issued command starting with - is assumed to be an acme.sh
    sub-command. This avoids having to maintain an up-to-date list of all
    commands in the entry.sh script.

--- docker/entry.sh
+++ docker/entry.sh
@@ -2,17 +2,16 @@
 
 set -e
 
-: "${LE_CONFIG_HOME:=/acme.sh}"
-export LE_CONFIG_HOME="$LE_CONFIG_HOME"
+export LE_CONFIG_HOME="${LE_CONFIG_HOME:=/acme.sh}"
+
+if [ "${1:0:1}" = '-' ]; then
+  set -- /root/.acme.sh/acme.sh --config-home "$LE_CONFIG_HOME" "$@"
+fi
 
 case "$1" in
---version | --install | --uninstall | --upgrade | --issue | --signcsr | --deploy | --install-cert | --renew | --renew-all | --revoke | --remove | --list | --showcsr | --install-cronjob | --uninstall-cronjob | --cron | --toPkcs | --toPkcs8 | --update-account | --register-account | --create-account-key | --create-domain-key | --createCSR | --deactivate | --deactivate-account | --set-notify)
-  exec /root/.acme.sh/acme.sh --config-home "$LE_CONFIG_HOME" "$@"
-  ;;
 daemon)
-  exec crond -l 2 -f
-  ;;
-*)
-  exec "$@"
+  set -- crond -l 2 -f
   ;;
 esac
+
+exec "$@"
